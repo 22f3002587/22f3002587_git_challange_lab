6fb2f8d7b93eecf516b6c392b19c8ed8441fbe47
22f3002587 <22f3002587@ds.study.iitm.ac.in>
2026-01-21 14:26:50 +0530

    Added add route which gives sum of two numbers

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
 
+app.route("/add<int:a><int:b>", methods=["POST"])
+def add(a, b):
+    return {"message":"sum of {a} and {b} is :{a + b}"}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
